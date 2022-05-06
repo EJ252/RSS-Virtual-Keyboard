7b9a19f70a32333aaa41b18833f7e6c0a83b56f3
EJ252 <w660@live.ru>
2022-05-06 22:21:49 +0300

    feat: add design and creation html in js

--- src/index.js
+++ src/index.js
@@ -12,11 +12,46 @@ const Keyboard = {
 
     this.elements.main.classList.add('keyboard');
     this.elements.keyboardBody.classList.add('keyboard__body');
+    this.elements.keyboardBody.appendChild(this._createKeys());
 
     this.elements.main.appendChild(this.elements.keyboardBody);
     document.body.appendChild(this.elements.main);
 
 
-  }
+  },
+  _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+      'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 
+      'caps lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', `'`, 'enter',
+      'shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '?', 'shift ',
+      'ctrl', 'win', 'alt', 'space', 'alt', 'ctrl', 'left', 'up', 'down', 'right', 'del'
+    ];
+    
+    const keyLayoutSpecial = keyLayout.filter(key => key.length > 1 && key !== 'caps lock' && key !== 'shift ');
+
+
+    keyLayout.forEach(x => {
+      const keyElement = document.createElement('button');
+      keyElement.setAttribute('type', 'button');
+      keyElement.classList.add('keyboard__key');
+
+      const arrow = ['left', 'up', 'down', 'right'].indexOf(x) === -1;
+      if (arrow) keyElement.textContent = x; // add text in button from keyLayout
+
+      fragment.appendChild(keyElement);
+
+      if(keyLayoutSpecial.includes(x) || x === '\\') keyElement.classList.add(`keyboard__${x}`, 'keyboard__special');
+
+      if(x === 'caps lock') keyElement.classList.add('keyboard__caps-lock', 'keyboard__key-light', 'keyboard__special');
+
+      if(x === 'shift ') keyElement.classList.add('keyboard__shiftR', 'keyboard__special');
+
+    });
+
+    return fragment;
+
+  },
 };
 window.addEventListener('DOMContentLoaded', Keyboard.init());

cf3e309ec814feb585c1a7832fbe29f4a4663bb5
EJ252 <w660@live.ru>
2022-05-07 10:12:06 +0300

    feat: add russian

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const Keyboard = {
     keysSpecial: [],
   },
 
+  keyCodeArr: [
+    'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 
+    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 
+    'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 
+    'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 
+    'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight', 'Delete'
+  ],
+
   init() {
     this.elements.main = document.createElement('div');
     this.elements.keyboardBody = document.createElement('div');
@@ -14,20 +22,40 @@ const Keyboard = {
     this.elements.keyboardBody.classList.add('keyboard__body');
     this.elements.keyboardBody.appendChild(this._createKeys());
 
+    this.elements.keys = this.elements.keyboardBody.querySelectorAll('.keyboard__key');
+
     this.elements.main.appendChild(this.elements.keyboardBody);
     document.body.appendChild(this.elements.main);
 
+    const area = document.body.querySelector('.text-area');
+    document.body.addEventListener('keyup', (e) => {
+      console.log(e)
+      if (e.key.length < 3 && this.keyCodeArr.includes(e.code)) area.value += e.key;
+      console.log(String.fromCharCode(e.key.charCodeAt()));
+      if (e.code === 'ControlLeft') this.elements.keyboardBody.appendChild(this._createKeys('rus'));
+    })
 
   },
-  _createKeys() {
+  _createKeys(lang) {
+    this.elements.keyboardBody.innerHTML = '';
     const fragment = document.createDocumentFragment();
-    const keyLayout = [
+    let keyLayout = [
       '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
       'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 
       'caps lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', `'`, 'enter',
       'shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '?', 'shift ',
       'ctrl', 'win', 'alt', 'space', 'alt', 'ctrl', 'left', 'up', 'down', 'right', 'del'
     ];
+
+    if(lang === 'rus') {
+      keyLayout = [
+        'Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+        'tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 
+        'caps lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', `Э`, 'enter',
+        'shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', 'shift ',
+        'ctrl', 'win', 'alt', 'space', 'alt', 'ctrl', 'left', 'up', 'down', 'right', 'del'
+      ]; 
+    }
     
     const keyLayoutSpecial = keyLayout.filter(key => key.length > 1 && key !== 'caps lock' && key !== 'shift ');
 
